550431b67602c9af958b6cc880205ca8d9de1269
Miguel Angel Alvarez <mijelito333@gmail.com>
2024-07-10 13:17:30 -0600

    Cambios del fronEnd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { ListPageComponent } from './citas/pages/list-page/list-page.component';
+import {NewPageComponent} from './citas/pages/new-page/new-page.component';
+
+// Angular Material Modules
 import { MatIconModule } from '@angular/material/icon';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
@@ -17,26 +20,28 @@ import { MatCardModule } from '@angular/material/card';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { ListPageComponent } from './citas/pages/list-page/list-page.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { MatMenu, MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
-import { MatSidenavContainer } from '@angular/material/sidenav';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatTableModule } from '@angular/material/table';
+import { MatCalendar } from '@angular/material/datepicker'; // Ensure this is correct or adjust based on your structure
+
 @NgModule({
   declarations: [
-    AppComponent,
-  
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    MatIconModule,
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
+    // Angular Material Modules
+    MatIconModule,
     MatInputModule,
+    MatButtonModule,
     MatSelectModule,
     MatDatepickerModule,
     MatNativeDateModule,
@@ -45,14 +50,10 @@ import { MatTableModule } from '@angular/material/table';
     MatTooltipModule,
     MatToolbarModule,
     MatFormFieldModule,
-    MatButtonModule,
     MatGridListModule,
     MatMenuModule,
-    MatButtonModule,
-    MatSidenavContainer,
+    MatSidenavModule,
     MatTableModule
-    
-    
   ],
   providers: [
     provideAnimationsAsync()
